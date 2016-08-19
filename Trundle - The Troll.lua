568f9538442265586d58dd121704ba7bb16f10de
Quackyy <diegosolteirinho992@gmail.com>
2016-08-19 13:21:07 +0100

    Trundle- The Troll
    Combo : Auto Attack Reset
    (Trundle will auto attack use Q and auto attack and use one of the following items [Tiamat,TitanicHydra,RaveneousHydra])
    
    Anti-Gapcloser 
    (If a Lee Sin or other champion with dashes engage on you trundle will use his E to stop it)  
    
    Trundle Skin Changer
    (You can choose to play with one of the following skins [Classic, Lil'Slugger, Junkyard, Traditional, Constable])

--- Trundle - The Troll.lua
+++ Trundle - The Troll.lua
@@ -10,6 +10,8 @@ function AutoUpdate(data)
 end
 GetWebResultAsync("https://raw.githubusercontent.com/Quackyy/TrundleScript/master/Trundle%20-%20The%20Troll.version", AutoUpdate)
   
+  local movePos = GetPrediction(GetCurrentTarget(),{delay = 0.5, speed = math.huge, width = 50, range = math.huge}).castPos
+  
   if Mode() == "Combo" then 
     if TrundleMenu.Gapclose.GCW:Value() and Ready(_W) and ValidTarget(target, 1000) then
       if GetDistance(myHero, target) > 340 and GetDistance(myHero, target) < 1000 and GetDistance(movePos) > GetDistance(target) then
